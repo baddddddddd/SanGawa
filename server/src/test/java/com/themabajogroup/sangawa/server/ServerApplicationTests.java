b8ccc2af495749ed9ecba2a7c91a774f29349a1b
Eluxe <eluxeofficial@gmail.com>
2024-11-16 14:38:12 +0800

    Initialize project for server

---
+++ server/src/test/java/com/themabajogroup/sangawa/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.themabajogroup.sangawa.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
