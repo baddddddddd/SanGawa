2eb57ada1888bbdd5f421e400bc6446453db7103
Eluxe <eluxeofficial@gmail.com>
2024-11-24 10:45:24 +0800

    Implement CRUD operations for singular tasks

---
+++ client/app/src/main/java/com/themabajogroup/sangawa/Controllers/TaskController.java
@@ -0,0 +1,119 @@
+package com.themabajogroup.sangawa.Controllers;
+
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.themabajogroup.sangawa.Models.TaskDetails;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+import kotlin.NotImplementedError;
+
+public class TaskController {
+    private static TaskController instance;
+    private final FirebaseFirestore db;
+
+    private TaskController() {
+        db = FirebaseFirestore.getInstance();
+    }
+
+    public static TaskController getInstance() {
+        if (instance == null) {
+            instance = new TaskController();
+        }
+
+        return instance;
+    }
+
+    public CompletableFuture<Boolean> createUserTask(TaskDetails taskDetails) {
+        CompletableFuture<Boolean> result = new CompletableFuture<>();
+
+        db.collection("tasks")
+                .add(taskDetails.toMap())
+                .addOnCompleteListener(task -> {
+                    result.complete(task.isSuccessful());
+                });
+
+        return result;
+    }
+
+    public CompletableFuture<TaskDetails> getUserTask(String taskId) {
+        CompletableFuture<TaskDetails> result = new CompletableFuture<>();
+
+        db.collection("tasks")
+                .document(taskId)
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        DocumentSnapshot document = task.getResult();
+
+                        if (document.exists()) {
+                            TaskDetails taskDetails = TaskDetails.fromDocumentSnapshot(document);
+                            result.complete(taskDetails);
+                        } else {
+                            // TK: handle case when task doesn't exist
+                        }
+                    } else {
+                        // TK: handle case when task failed
+                    }
+                });
+
+        return result;
+    }
+
+    public CompletableFuture<Boolean> editUserTask(String taskId, TaskDetails updatedTask) {
+        CompletableFuture<Boolean> result = new CompletableFuture<>();
+
+        db.collection("tasks")
+                .document(taskId)
+                .update(updatedTask.toMap())
+                .addOnCompleteListener(task -> {
+                   result.complete(task.isSuccessful());
+                });
+
+        return result;
+    }
+
+    public CompletableFuture<Boolean> deleteUserTask(String taskId) {
+        CompletableFuture<Boolean> result = new CompletableFuture<>();
+
+        db.collection("tasks")
+                .document(taskId)
+                .delete()
+                .addOnCompleteListener(task -> {
+                    result.complete(task.isSuccessful());
+                });
+
+        return result;
+    }
+
+    public CompletableFuture<List<TaskDetails>> getAllUserTasks(String userId) {
+        CompletableFuture<List<TaskDetails>> result = new CompletableFuture<>();
+
+        db.collection("tasks")
+                .whereEqualTo("userId", userId)
+                .get()
+                .addOnCompleteListener(task -> {
+                   if (task.isSuccessful()) {
+                       List<DocumentSnapshot> documents = task.getResult().getDocuments();
+                       ArrayList<TaskDetails> tasks = new ArrayList<>();
+
+                       for (DocumentSnapshot document : documents) {
+                           TaskDetails taskDetails = TaskDetails.fromDocumentSnapshot(document);
+                           tasks.add(taskDetails);
+                       }
+
+                       result.complete(tasks);
+                   } else {
+                       // TK: handle case when task failed
+                   }
+                });
+
+        return result;
+    }
+
+    public List<TaskDetails> getNearbyTasks(double lat, double lon) {
+        throw new NotImplementedError();
+    }
+}
--- client/app/src/main/java/com/themabajogroup/sangawa/Models/TaskDetails.java
+++ client/app/src/main/java/com/themabajogroup/sangawa/Models/TaskDetails.java
@@ -1,5 +1,8 @@
 package com.themabajogroup.sangawa.Models;
 
+import com.google.firebase.Timestamp;
+import com.google.firebase.firestore.DocumentSnapshot;
+
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -14,6 +17,36 @@ public class TaskDetails {
     private double locationLon;
     private TaskVisibility visibility;
     private TaskStatus status;
+
+    public TaskDetails(String userId, String title, String description, double locationLat, double locationLon, TaskVisibility visibility, Date deadline) {
+        this.userId = userId;
+        this.title = title;
+        this.description = description;
+        this.locationLat = locationLat;
+        this.locationLon = locationLon;
+        this.visibility = visibility;
+        this.status = TaskStatus.PENDING;
+        this.deadline = deadline;
+        this.dateCreated = new Date();
+    }
+
+    public static TaskDetails fromDocumentSnapshot(DocumentSnapshot documentSnapshot) {
+        Map<String, Object> data = documentSnapshot.getData();
+        TaskDetails taskDetails = new TaskDetails(
+                (String) data.get("userId"),
+                (String) data.get("title"),
+                (String) data.get("description"),
+                (Double) data.get("locationLat"),
+                (Double) data.get("locationLon"),
+                TaskVisibility.valueOf((String) data.get("visibility")),
+                ((Timestamp) data.get("deadline")).toDate()
+        );
+
+        taskDetails.setDateCreated(((Timestamp) data.get("dateCreated")).toDate());
+        taskDetails.setStatus(TaskStatus.valueOf((String) data.get("status")));
+
+        return taskDetails;
+    }
 
     public String getUserId() {
         return userId;
@@ -100,5 +133,20 @@ public class TaskDetails {
         map.put("visibility", getVisibility().name());
         map.put("status", getStatus().name());
         return map;
+    }
+
+    @Override
+    public String toString() {
+        return "TaskDetails{" +
+                "userId='" + userId + '\'' +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", deadline=" + deadline +
+                ", dateCreated=" + dateCreated +
+                ", locationLat=" + locationLat +
+                ", locationLon=" + locationLon +
+                ", visibility=" + visibility +
+                ", status=" + status +
+                '}';
     }
 }
