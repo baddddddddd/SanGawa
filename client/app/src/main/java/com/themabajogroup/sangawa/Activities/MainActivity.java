47c1271cfb348e834e91fd0fd8ad1051feb4b202
Deogenes Maranan <123554998+DeogenesMaranan@users.noreply.github.com>
2024-12-01 23:30:04 +0800

    Revert splash screen optimization to fix glitches and adjust delay

--- client/app/src/main/java/com/themabajogroup/sangawa/Activities/MainActivity.java
+++ client/app/src/main/java/com/themabajogroup/sangawa/Activities/MainActivity.java
@@ -19,9 +19,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
+
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.main).post(this::redirectToAuthActivity);
+        findViewById(R.id.main).postDelayed(() -> {
+            Intent intent = new Intent(MainActivity.this, AuthActivity.class);
+            startActivity(intent);
+            finish();
+        }, 1000);
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
@@ -30,12 +35,6 @@ public class MainActivity extends AppCompatActivity {
         });
 
         new Thread(this::createNotificationChannel).start();
-    }
-
-    private void redirectToAuthActivity() {
-        Intent intent = new Intent(MainActivity.this, AuthActivity.class);
-        startActivity(intent);
-        finish();
     }
 
     private void createNotificationChannel() {
