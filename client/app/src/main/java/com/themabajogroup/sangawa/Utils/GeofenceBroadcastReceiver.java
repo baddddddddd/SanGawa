47389694ec8e9c8fd337b509be1b34eae2e36296
Eluxe <eluxeofficial@gmail.com>
2024-12-03 05:20:43 +0800

    Update Firestore on collab reply

--- client/app/src/main/java/com/themabajogroup/sangawa/Utils/GeofenceBroadcastReceiver.java
+++ client/app/src/main/java/com/themabajogroup/sangawa/Utils/GeofenceBroadcastReceiver.java
@@ -5,11 +5,9 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
-import androidx.core.app.NotificationCompat;
 
 import com.google.android.gms.location.Geofence;
 import com.google.android.gms.location.GeofencingEvent;
-import com.themabajogroup.sangawa.R;
 
 import java.util.List;
 
@@ -32,37 +30,18 @@ public class GeofenceBroadcastReceiver extends BroadcastReceiver {
             Log.e("GeofenceBroadcast", "Error in geofence event: " + geofencingEvent.getErrorCode());
             return;
         }
-
+        NotificationSender sender = NotificationSender.getInstance("GeofenceNotifcations", context);
         int geofenceTransition = geofencingEvent.getGeofenceTransition();
         if (geofenceTransition == Geofence.GEOFENCE_TRANSITION_ENTER) {
-            // TODO: Notify when user enters a geofence of a task
-            // TODO: Add task to "Nearby Tasks"
-
             List<Geofence> triggeringGeofences = geofencingEvent.getTriggeringGeofences();
 
             for (Geofence geofence : triggeringGeofences) {
                 String geofenceId = geofence.getRequestId();
-                sendNotification(context, "Geofence Entered: " + geofenceId, "You have entered the geofence area!");
+                sender.sendNotification("Geofence Entered: " + geofenceId, "You have entered the geofence area!");
             }
 
         } else if (geofenceTransition == Geofence.GEOFENCE_TRANSITION_EXIT) {
-            // TODO: Remove task from "Nearby Tasks"
-            sendNotification(context, "Geofence Exited", "You have exited the geofence area!");
-        }
-    }
-
-    private void sendNotification(Context context, String title, String message) {
-        // TODO: Use NotificationSender class instead
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "GeofenceChannel")
-                .setSmallIcon(R.drawable.logo)
-                .setContentTitle(title)
-                .setContentText(message)
-                .setPriority(NotificationCompat.PRIORITY_HIGH)
-                .setAutoCancel(true);
-
-        NotificationManager manager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-        if (manager != null) {
-            manager.notify(1, builder.build());
+            sender.sendNotification("Geofence Exited", "You have exited the geofence area!");
         }
     }
 }
